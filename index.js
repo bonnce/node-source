26804923e4baa99c5d4265b81721f03e7dc73141
bonnce <bonce_reloko@hotmail.com>
2023-05-18 17:32:52 -0300

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = 3500
+
+app.get('/',(req_,res)=>{
+    res.send('Hello world!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
