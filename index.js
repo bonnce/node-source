1f284804932130efccebcba34ea08b06af4babd1
bonnce <bonce_reloko@hotmail.com>
2023-06-08 23:20:49 -0300

    vamo a juga

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3500
 
 app.get('/',(req_,res)=>{
-    res.send('Hello world!! esto no es una prueba')
+    res.send('Hello world!!')
 })
 
 app.listen(port, () => {
